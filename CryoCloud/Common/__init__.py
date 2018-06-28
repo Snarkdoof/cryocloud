b1d420914385b8e5792a236ffcd24feffe6ab8aa
Njål Borch <njaal@njaal.net>
2018-06-28 10:33:28 +0200

    Warn but don't freak out on import

--- CryoCloud/Common/__init__.py
+++ CryoCloud/Common/__init__.py
@@ -1,2 +1,9 @@
-from .directorywatcher import DirectoryWatcher
-from .netwatcher import NetWatcher
+try:
+    from .directorywatcher import DirectoryWatcher
+except Exception as e:
+    print("Warning: Not able to import directory watcher", e)
+
+try:
+    from .netwatcher import NetWatcher
+except Exception as e:
+    print("Warning: Not able to import net watcher", e)
