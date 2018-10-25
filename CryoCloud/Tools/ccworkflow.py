342fcc117a6c1aa9f6f63cc90ed8c1d56c4c3d6d
Njål <njaal.borch@norut.no>
2018-10-25 15:27:59 +0200

    Ignore errors in global erro rhandlers

--- CryoCloud/Tools/ccworkflow.py
+++ CryoCloud/Tools/ccworkflow.py
@@ -1174,10 +1174,13 @@ class WorkflowHandler(CryoCloud.DefaultHandler):
         workflow.nodes[node].on_completed(pebble, "error")
 
         # Do we have any global error handlers (and is THIS one of them?)
-        if not pebble.is_sub_pebble and not workflow.nodes[node].is_global:
-            for g in workflow.global_nodes:
-                print("*** Error and not a sub-pebble, reporting as global error")
-                g.resolve(pebble, "error", workflow.nodes[node])
+        try:
+            if not pebble.is_sub_pebble and not workflow.nodes[node].is_global:
+                for g in workflow.global_nodes:
+                    print("*** Error and not a sub-pebble, reporting as global error")
+                    g.resolve(pebble, "error", workflow.nodes[node])
+        except:
+            self.log.exception("Error in global error handler, ignoring")
 
         self._flag_cleanup_pebble(pebble)
 
