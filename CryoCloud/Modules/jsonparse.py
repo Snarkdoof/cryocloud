6c51cd78ef560d40f469c46fcd5ac5a4109cbe57
Njål Borch <njaal.borch@norut.no>
2019-06-19 13:52:44 +0200

    Added module to read json

---
+++ CryoCloud/Modules/jsonparse.py
@@ -0,0 +1,39 @@
+import os
+import json
+
+ccmodule = {
+    "description": "Read a json file and return the contents as output",
+    "depends": [],
+    "provides": [],
+    "inputs": {
+        "path": "Full path to input file"
+    },
+    "outputs": {
+        "content": "The content of the file"
+    },
+    "defaults": {
+        "priority": 0,  # Bulk
+        "runOn": "always"
+    }
+}
+
+
+def start(handler, args):
+
+    if "path" not in args:
+        raise Exception("Required argument 'path' not given")
+    filename = args["path"]
+    if not os.path.exists(filename):
+        raise Exception("Missing file '%s'" % filename)
+
+    try:
+        data = open(filename, "r").read()
+    except Exception as e:
+        raise Exception("Failed to read file '%s': %s" % (filename, str(e)))
+
+    try:
+        content = json.loads(data)
+    except Exception as e:
+        raise Exception("Failed to parse json from file '%s': %s" % (filename, str(e)))
+
+    return {"progress": 100, "content": content}
