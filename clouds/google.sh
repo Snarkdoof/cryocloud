5eae1e5f842920c6c2877722e4b35b592c595bb8
Njål <njaal.borch@norut.no>
2020-10-08 15:44:30 +0200

    Added more stuff

--- clouds/google.sh
+++ clouds/google.sh
@@ -6,7 +6,7 @@ cd /home/cryocore/git/cryocloud
 git pull
 
 # Now we're current, update all repos
-/home/cryocore/git/cryocloud/clouds/update_repos.sh
+/home/cryocore/git/cryocloud/clouds/update_repos
 
 # Start ccnodes
 screen -d ccnode
