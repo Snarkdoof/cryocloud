1e7b16cc69d882e96fe4423ed68e6ba91b689525
Njål <njaal.borch@norut.no>
2020-10-08 15:35:00 +0200

    Cloud setup script for google cloud nodes

---
+++ clouds/google.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Startup script for Google Cloud ccnodes"
+
+cd /home/cryocore/git/cryocloud
+git pull
+
+# Now we're current, update all repos
+/home/cryocore/git/cryocloud/clouds/update_repos.sh
+
+# Start ccnodes
+screen -d ccnode
+
+# Check for GPUs and start GPU nodes too
