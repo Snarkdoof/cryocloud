8ed8a9e8fd313b17392b21c9a2e3a6f29ac6140b
Njål <njaal.borch@norut.no>
2020-10-09 13:39:35 +0200

    Multiple repos at once also

--- CryoCloud/Modules/git.py
+++ CryoCloud/Modules/git.py
@@ -14,7 +14,8 @@ ccmodule = {
         "src": "Source URL of repository",
         "dst": "Destination directory, e.g. /home/cryocore/git",
         "name": "Repository name, will be cloned into dst/name",
-        "branch": "Branch to check out, default 'master'"
+        "branch": "Branch to check out, default 'master'",
+        "list": "JSON list of tuples [(name, url, branch)] to update for multiple"
     },
     "outputs": {
         "branch": "Branch that was checked out"
@@ -39,39 +40,40 @@ def canrun():
 
 def process_task(worker, task):
     args = task["args"]
-    for item in ["src", "dst", "name"]:
-        if item not in args:
-            raise Exception("Missing %s" % item)
+    if "list" not in args:
+        for item in ["src", "dst", "name"]:
+            if item not in args:
+                raise Exception("Missing %s" % item)
+        things = [args["name"], args["src"], args.get("branch", "master")]
+    else:
+        things = args["list"]
 
-    src = args["src"]
     dst = args["dst"]
-    name = args["name"]
-    branch = args.get("branch", "master")
-
     if not os.path.exists(dst):
         os.makedirs(dst)
     olddir = os.getcwd()
     try:
-        os.chdir(dst)
-        dst_path = os.path.join(dst, name)
-        if not os.path.exists(dst_path):
-            # Must clone it
-            worker.status["state"] = "cloning"
-            worker.log.info("Cloning %s from %s" % (name, src))
-            cmd = ["git", "clone", src, dst_path]
+        for name, src, branch in things:
+            os.chdir(dst)
+            dst_path = os.path.join(dst, name)
+            if not os.path.exists(dst_path):
+                # Must clone it
+                worker.status["state"] = "cloning %s" % name
+                worker.log.info("Cloning %s from %s" % (name, src))
+                cmd = ["git", "clone", src, dst_path]
+                if subprocess.call(cmd) != 0:
+                    raise Exception("Clone of %s failed" % src)
+
+            worker.status["state"] = "updating %s" % name
+            os.chdir(dst_path)
+            cmd = ["git", "checkout", branch]
             if subprocess.call(cmd) != 0:
-                raise Exception("Clone of %s failed" % src)
+                raise Exception("Checkout branch %s of %s failed" % (branch, name))
 
-        worker.status["state"] = "updating"
-        os.chdir(dst_path)
-        cmd = ["git", "checkout", branch]
-        if subprocess.call(cmd) != 0:
-            raise Exception("Checkout branch %s of %s failed" % (branch, name))
-
-        # Update
-        cmd = ["git", "pull"]
-        if subprocess.call(cmd) != 0:
-            raise Exception("Checkout pull of %s failed" % (name))
+            # Update
+            cmd = ["git", "pull"]
+            if subprocess.call(cmd) != 0:
+                raise Exception("Checkout pull of %s failed" % (name))
 
         worker.status["state"] = "idle"
     finally:
