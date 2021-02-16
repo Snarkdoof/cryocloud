7ef8c2bade69ae7f21b98bb057847d6b46c9e0f3
Njål Borch <njbo@norceresearch.no>
2021-02-16 10:06:01 +0100

    Added faux search

--- ciopcc/fakeciop.py
+++ ciopcc/fakeciop.py
@@ -12,3 +12,11 @@ class Cioppy:
         print("CIOP copying file")
         return os.path.join("/tmp", source)
 
+    def getparam(self, param):
+        params = {
+            "aoi": "POLYGON ((-98.5 18.9, -98.5 19.3, -99.2 19.3, -99.2 18.9, -98.5 18.9))",
+            "start_date": "2020-08-01T00:00Z",
+            "end_date": "2020-08-21T00:00Z"
+        }
+
+        return params.get(param, None)
